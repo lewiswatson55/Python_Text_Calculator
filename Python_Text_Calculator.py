63a3a5addd45550b3131270241a7034091b6b136
Shankhalika Sarkar <shankhalika.s@gmail.com>
2017-10-15 10:37:30 +0530

    Added modulo operator

--- Python_Text_Calculator.py
+++ Python_Text_Calculator.py
@@ -37,6 +37,24 @@ def Calculator():
         print("Answer: ", number1 + number2)
         print("")
         Calculator();
+        
+        
+    elif calc == "%":
+        try:
+            number1 = int(input("Please select the first number(greater): "))
+            number2 = int(input("Please select the second number(smaller): "))
+        except (TypeError, ValueError):
+            print("Invalid input")
+            print("")
+            Calculator();        
+        if(abs(number1)<abs(number2)):
+           print("")
+           print("The second number entered is greater than the bigger number")
+           Calculator();
+        print("Answer: ", number1-number2*int(number1/number2))
+        print("")
+        Calculator();
+        
 	
     elif calc == "exit":
         exit();
