5a7765ef0dd3ee985426c980f44d0802f26fe986
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-06-27 15:49:03 +0200

    Refactor and cleanup
    Signed-off-by: pBouillon <pro.pierre.bouillon@protonmail.com>

--- text_calculator.py
+++ text_calculator.py
@@ -2,79 +2,68 @@ from operations import *
 
 
 def calculator():
-    global number2, number1
     calc = input(
-        f"\nWhat kind of calculation do you wish to do? "
-        f"(type {OP_HELP} for help): "
+        f'\nWhat kind of calculation do you wish to do? '
+        f'(type {OP_HELP} for help): '
     )
 
+    # base commands
     if calc == OP_HELP:
         display_help()
-        calculator()
-
-    elif calc == OP_MULT:
-        number1 = get_input()
-        number2 = get_input()
-        print("Answer: ", number1 * number2)
-
-    elif calc == OP_SQUARE:
-        number1 = get_input()
-        print("Answer: ", number1 * number1)
-
-    elif calc == OP_DIV:
-        number1 = get_input()
-        number2 = get_input()
-        print("Answer: ", number1 / number2)
-
-    elif calc == OP_SUB:
-        number1 = get_input()
-        number2 = get_input()
-        print("Answer: ", number1 - number2)
-
-    elif calc == OP_ADD:
-        number1 = get_input()
-        number2 = get_input()
-        print("Answer: ", number1 + number2)
-
-    elif calc == OP_MOD:
-        number1 = get_input()
-        number2 = get_input()
-
-        if number1 < number2:
-            print("The second number entered is greater than the bigger number")
-
-        print("Answer: ", number1 - number2 * int(number1 / number2))
 
     elif calc == OP_EXIT:
         exit()
 
-    else:
-        print("Sorry, I dont understand your request. ")
+    elif calc not in ALLOWED_OPERATIONS:
+        print('Sorry, I dont understand your request. ')
         display_help()
-        print("Sorry for the inconvenience!")
+        print('Sorry for the inconvenience!\n')
 
-    print()
+    else:
+        # operations with one number
+        res = get_input()
+        if calc == OP_SQUARE:
+            res *= res
+
+        # operations with two numbers
+        else:
+            number2 = get_input(index=2)
+            if calc == OP_MULT:
+                res *= number2
+
+            elif calc == OP_DIV:
+                res //= number2
+
+            elif calc == OP_SUB:
+                res -= number2
+
+            elif calc == OP_ADD:
+                res += number2
+
+            elif calc == OP_MOD:
+                if res < number2:
+                    print('The second number entered is greater than the bigger number')
+                res %= number2
+
+        print(f'Answer: {res}\n')
     calculator()
 
 
 def display_help():
-    print(f'Currently supported: {", ".join(sorted(ALLOWED_OPERATIONS))}')
+    print(f"Currently supported: {', '.join(sorted(ALLOWED_OPERATIONS))}")
 
 
-def get_input() -> int:
-    nb = 0
-    invalid = True
+def get_input(index=1) -> int:
+    msg = 'Please select the '
+    msg += 'first' if index == 1 else 'second'
+    msg += ' number: '
 
-    while invalid:
-        invalid = False
-        try:
-            nb = int(input("Please select the first number(greater): "))
-        except (TypeError, ValueError):
-            print("Invalid input")
+    nb = ''
+    while not nb.isdigit():
+        nb = input(msg)
 
-    return nb
+    return int(nb)
 
 
 if __name__ == '__main__':
-    print("")
     calculator()
