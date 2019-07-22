f6009ebc97698b78204fe181312cabb1ec166757
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-22 15:42:10 -0400

    0.1
    TheTechRobo Fork 0.1
    Defragged and potentially reduced filesize

--- Python_Text_Calculator.py
+++ Python_Text_Calculator.py
@@ -1,84 +1,73 @@
+print("TheTechRobo Version: 0.1")
 def Calculator():
+    while True: 
 
-    calc = input("What kind of calculation do you wish to do? (type ? for help): ")
+        calc = input("What kind of calculation do you wish to do? (type ? for help): ")
 
-    if calc == "?":
-        print("Currently supported: multiplication(*), division(/), addition(+), square (sq), subtraction (-) and modulo (%)")
-        print("")
-        Calculator();
-
-    elif calc == "*":
-        print("")
-        number1 = int(input("Please select the first number: "))
-        number2 = int(input("Please select the second number: "))
-        print("Answer: ", number1 * number2)
-        print("")
-        Calculator();
-
-    elif calc == "sq":
-        print("")
-        number1 = int(input("Please select the first number: "))
-        print("Answer: ", number1 * number1)
-        print("")
-        Calculator();
-
-
-
-    elif calc == "/":
-        print("")
-        number1 = int(input("Please select the first number: "))
-        number2 = int(input("Please select the second number: "))
-
-        print("Answer: ", number1 / number2)
-        print("")
-        Calculator();
-
-    elif calc == "-":
-        print("")
-        number1 = int(input("Please select the first number: "))
-        number2 = int(input("Please select the second number: "))
-
-        print("Answer: ", number1 - number2)
-        print("")
-        Calculator();
-
-    elif calc == "+":
-        print("")
-        number1 = int(input("Please select the first number: "))
-        number2 = int(input("Please select the second number: "))
-
-        print("Answer: ", number1 + number2)
-        print("")
-        Calculator();
-
-
-    elif calc == "%":
-        print("")
-        try:
-            number1 = int(input("Please select the first number(greater): "))
-            number2 = int(input("Please select the second number(smaller): "))
-        except (TypeError, ValueError):
-            print("Invalid input")
+        if calc == "?":
+            print("Currently supported: multiplication(*), division(/), addition(+), square (sq), subtraction (-) and modulo (%)")
             print("")
-            Calculator();
-        if(abs(number1)<abs(number2)):
-           print("")
-           print("The second number entered is greater than the bigger number")
-           print("")
-           Calculator();
-        print("Answer: ", number1-number2*int(number1/number2))
-        print("")
-        Calculator();
+
+        elif calc == "*":
+            print("")
+            number1 = int(input("Please select the first number: "))
+            number2 = int(input("Please select the second number: "))
+            print("Answer: ", number1 * number2)
+            print("")
+
+        elif calc == "sq":
+            print("")
+            number1 = int(input("Please select the first number: "))
+            print("Answer: ", number1 * number1)
+            print("")
 
 
-    elif calc == "exit":
-        exit();
 
-    else:
-        print("")
-        print("Sorry, I don't understand your request. Currently supported calculations: *, /, -, + and % (MODULO). Sorry for the inconvenience!")
-        print("")
-        Calculator();
+        elif calc == "/":
+            print("")
+            number1 = int(input("Please select the first number: "))
+            number2 = int(input("Please select the second number: "))
+            print("Answer: ", number1 / number2)
+            print("")
+
+        elif calc == "-":
+            print("")
+            number1 = int(input("Please select the first number: "))
+            number2 = int(input("Please select the second number: "))
+            print("Answer: ", number1 - number2)
+            print("")
+
+        elif calc == "+":
+            print("")
+            number1 = int(input("Please select the first number: "))
+            number2 = int(input("Please select the second number: "))
+            print("Answer: ", number1 + number2)
+            print("")
+
+
+        elif calc == "%":
+            print("")
+            try:
+                number1 = int(input("Please select the first number(greater): "))
+                number2 = int(input("Please select the second number(smaller): "))
+            except (TypeError, ValueError):
+                print("Invalid input")
+                print("")
+            if(abs(number1)<abs(number2)):
+                print("")
+                print("The second number entered is greater than the bigger number")
+                print("")
+                Calculator();
+                print("Answer: ", number1-number2*int(number1/number2))
+                print("")
+                
+        elif calc == "exit":
+            exit();
+
+        else:
+            print("")
+            print("Sorry, I don't understand your request. Currently supported calculations: *, /, -, + and % (MODULO). Sorry for the inconvenience!")
+            print("")
 
 print("")
 Calculator();
