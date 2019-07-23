02c311abb724aad48b0ad7e733e6c3ed65c56150
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-23 11:38:02 -0400

    0.3
    Plenty of new stuff!
    * (possible) bugfix
    * Volume and cube functions! 
    * Added a few more comments
    * Smaller filesize, to make room for...
    * Aliases for commands, e.g. type div for /

--- PyTxtCalc.py
+++ PyTxtCalc.py
@@ -1,7 +1,7 @@
-print("TheTechRobo Version: 0.2.6")
+print("TheTechRobo Version: 0.3")
 #SETUP
 import os
-#make a new command called Calculator()
+#make a new command called Calc()
 def Calc():
     while True: 
 #CALCULATION CHOICE
@@ -9,44 +9,83 @@ def Calc():
 #HELP
         if calc == "?":
             print('''
-            Currently supported: multiplication(*), division(/), addition(+), square (sq), subtraction (-), modulo (%), and area (ar). Type exit to exit. Commands are case-sensitive")
+            Currently supported: multiplication (* or x), division (/ or div), addition (+), square (sq), subtraction (-), modulo (%), area (ar), volume (vol), and cube ([]). Type exit to exit. Commands are case-sensitive")
             ''')
 #MULTIPLICATION
         elif calc == "*":
             print()
-            number1 = int(input("Please select the first number: "))
-            number2 = int(input("Please select the second number: "))
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("Type the second number: "))
             print()
             print(number1 * number2)
             print()
+            
+        elif calc == "x":
+            print()
+            number1 = int(input("First number? "))
+            number2 = int(input("Second number? "))
 #SQUARE
         elif calc == "sq":
             print()
-            number1 = int(input("Please select the first number: "))
+            squaredNumber = int(input("Type the number to be squared: "))
             print()
-            print(number1 * number1)
+            print(squaredNumber * squaredNumber)
+            print()
+        elif calc == "[]":
+            print()
+            squaredNumber = int(input("Type the number to be squared: "))
+            print()
+            print(squaredNumber * squaredNumber)
             print()
 #DIVISION
         elif calc == "/":
             print()
-            number1 = int(input("Please select the first number: "))
-            number2 = int(input("Please select the second number: "))
+            number1 = int(input("type the first number: "))
+            number2 = int(input("Type the second number: "))
+            print()
+            print(number1 / number2)
+            print()
+        elif calc == "div":
+            print()
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("Type the second number: "))
             print()
             print(number1 / number2)
             print()
 #SUBTRACTION
         elif calc == "-":
             print()
-            number1 = int(input("Please select the first number: "))
-            number2 = int(input("Please select the second number: "))
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("type the second number: "))
+            print()
+            print(number1 - number2)
+            print()
+        elif calc == "sub":
+            print()
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("type the second number: "))
+            print()
+            print(number1 - number2)
+            print()
+        elif calc == "min":
+            print()
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("type the second number: "))
             print()
             print(number1 - number2)
             print()
 #ADDITION
         elif calc == "+":
             print()
-            number1 = int(input("Please select the first number: "))
-            number2 = int(input("Please select the second number: "))
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("Type the second number: "))
+            print()
+            print(number1 + number2)
+            print()
+        elif calc == "add":
+            print()
+            number1 = int(input("Type the first number: "))
+            number2 = int(input("Type the second number: "))
             print()
             print(number1 + number2)
             print()
@@ -54,35 +93,79 @@ def Calc():
         elif calc == "%":
             print()
             try:
-                number1 = int(input("Please select the first number(greater): "))
-                number2 = int(input("Please select the second number(smaller): "))
+                number1 = int(input("Type the first number(greater): "))
+                number2 = int(input("Type the second number(smaller): "))
             except (TypeError, ValueError):
                 print("Error!")
                 print("Invalid input (code 1)")
                 print()
             if(abs(number1)<abs(number2)):
                 print()
-                print("The second number entered is greater than the bigger number")
+                print("The second number entered is greater than the first number")
                 print()
                 Calc()
+            else: 
                 print(number1-number2*int(number1/number2))
                 print()
-        
+        elif calc == "mod":
+            print()
+            try:
+                number1 = int(input("Type the first number(greater): "))
+                number2 = int(input("Type the second number(smaller): "))
+            except (TypeError, ValueError):
+                print("Error!")
+                print("Invalid input (code 1)")
+                print()
+            if(abs(number1)<abs(number2)):
+                print()
+                print("The second number entered is greater than the first number")
+                print()
+                Calc()
+            else: 
+                print(number1-number2*int(number1/number2))
+                print()
+#AREA        
         elif calc == "ar":
-            win = input("Is your OS the following: Windows? (y/n, case-sensitive)")
-            if win == "y":
+            win = input("Is your OS the following: Windows? (Y/n, case-sensitive)")
+            if win == "Y":
                 print()
                 os.run(start Area.bat)
             else: 
                 print()
                 print("Then this command is useless to you")
                 print()
+        elif calc == "#":
+            win = input("Is your OS the following: Windows? (Y/n, case-sensitive)")
+            if win == "Y":
+                print()
+                os.run(start Area.bat)
+            else: 
+                print()
+                print("Then this command is useless to you")
+                print()
+#VOLUME
+        elif calc == "vol":
+            win = input("Is your OS the following: Windows? (Y/n, case-sensitive)")
+            if win == "y":
+                print()
+                os.run(start Volume.bat)
+            else: 
+                print()
+                print("Then this command is useless to you")
+                print()
+#CUBE
+        elif calc == "{}":
+            print()
+            cubedNumber = int(input("Type the number to be cubed: "))
+            print()
+            print(cubedNumber * cubedNumber * cubedNumber)
+            print()
         elif calc == "exit":
             exit()
 
         else:
             print('''
-            print("Sorry, I don't understand your request. Currently supported calculations: *, /, -, +, % (modulo), sq (square) and ar (area). Sorry for the inconvenience")
+            print("Sorry, I don't understand your request. Currently supported calculations: * or x; / or div; -, min, or sub; + or add; % or mod (modulo); sq or [] (square); ar or # (area); vol (volume); and {} (cube). Sorry for the inconvenience")
             ''')
 
 print()
