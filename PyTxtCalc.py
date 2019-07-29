8b9219c682924cdabfa7f6045fe240f7fdbea13e
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-28 20:39:02 -0400

    Update PyTxtCalc.py
    • fix

--- PyTxtCalc.py
+++ PyTxtCalc.py
@@ -24,6 +24,9 @@ def Calc():
             print()
             number1 = int(input("First number? "))
             number2 = int(input("Second number? "))
+            print()
+            print(number1 * number2)
+            print()
 #SQUARE
         elif calc == "sq":
             print()
