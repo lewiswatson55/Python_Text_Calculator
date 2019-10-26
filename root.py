835c95f6653c214cefecfdbd27b05eac9e698316
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2019-10-26 19:22:13 -0400

    bugfix

--- root.py
+++ root.py
@@ -6,6 +6,6 @@ def cubeInternal(x):
     # all credit goes to user4466285's answer to "https://stackoverflow.com/questions/28014241/how-to-find-cube-root-using-python"
     if 0<=x: return x**(1./3.)
     return -(-x)**(1./3.)
-def cube():
+def cu():
     cubeInternal()
     print(int(cube(input("Number to be rooted? "))))
