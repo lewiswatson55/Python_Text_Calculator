82c1374ec1b6143e873f471c82f88d4efe3bc89c
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-12 19:49:29 -0400

    Update root.py
    space while doing 0.4.2 type

--- root.py
+++ root.py
@@ -1,7 +1,3 @@
-def sq():
-    from math import sqrt
-    num2Root = int(input("Number to be rooted? "))
-    print(sqrt(num2Root))
 def cubeInternal(x):
     # all credit goes to user4466285's answer to "https://stackoverflow.com/questions/28014241/how-to-find-cube-root-using-python"
     if 0<=x: return x**(1./3.)
