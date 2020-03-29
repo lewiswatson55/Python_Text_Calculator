a57964eb2ada62ba648609b28f778a37a1ac849e
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-03-29 14:34:58 -0400

    delete file

--- TEST.py
+++
@@ -1,9 +0,0 @@
-calc = "base"
-if calc == "base":
-    base = int(input('''What base would you like to use?
-Available: 2 (binary) 8 (octo) 10 (decimal (normal)) 16 (hex)
-Type 2, 8, 10, or 16: '''))
-    if base == 2:
-        result = bin(input("Type the original number: ")
-        printThis = "=" +str(result)
-        print(printThis)
