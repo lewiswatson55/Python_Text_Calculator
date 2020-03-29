ce97566365c6f102cd763b28c75a8b3a82032861
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-03-29 14:33:54 -0400

    Cleanup Clutter part 1

---
+++ func.py
@@ -0,0 +1,20 @@
+def 2num(): #ask for two numbers and then return to function
+    n1 = int(input("Please enter the first number: "))
+    n2 = int(input("Please enter the second number: "))
+    return n1, n2
+def multi(): #multiplication
+    2num()
+    print("\nThat equals...")
+    print(n1 * n2)
+def div(): #division
+    2num()
+    print("\nThat equals...")
+    print(n1 / n2)
+def sub(): #subtraction
+    2num()
+    print("\nThat equals...")
+    print(n1 - n2)
+def add(): #addition
+    2num()
+    print("\nThat equals...")
+    print(n1 + n2)
