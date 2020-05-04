871800a8aa091852d5dad95733d3b30e750b4127
Thittussarom <ittussarom@TtRTech-PC>
2020-05-04 16:27:00 -0400

    this is disrespect to francophones worldwide

--- areaInteractive.py
+++ areaInteractive.py
@@ -1,68 +1,68 @@
 from area import *
 def equ_triangle():
-    a = int(input("What length is the side of the triangle? "))
+    a = int(input(_("What length is the side of the triangle? ")))
     equtri(a)
 def right_triangle():
-    b = int(input("What length is the base of the triangle? "))
-    h = int(input("What length is the height of the triangle? "))
+    b = int(input(_("What length is the base of the triangle? ")))
+    h = int(input(_("What length is the height of the triangle? ")))
     righttri(b, h)
 def acute_triangle():
-    a = int(input("What is the length of the first side? "))
-    b = int(input("what is the length of the second side? "))
-    c = int(input("What is the length of the third side? "))
+    a = int(input(_("What is the length of the first side? ")))
+    b = int(input(_("what is the length of the second side? ")))
+    c = int(input(_("What is the length of the third side? ")))
     actri(a, b, c)
 def obtuse_triangle():
-    a = int(input("What is the length of the first side? "))
-    b = int(input("what is the length of the second side? "))
-    c = int(input("What is the length of the third side? "))
+    a = int(input(_("What is the length of the first side? ")))
+    b = int(input(_("what is the length of the second side? ")))
+    c = int(input(_("What is the length of the third side? ")))
     obtri(a, b, c)
 def square():
-    a = int(input("What is the length of the side of the square? "))
+    a = int(input(_("What is the length of the side of the square? ")))
     sq(a)
 def rectangle():
     from area import rectangle as rec
-    l = int(input("What is the length of the rectangle? "))
-    b = int(input("What is the height of the rectangle? "))
+    l = int(input(_("What is the length of the rectangle? ")))
+    b = int(input(_("What is the height of the rectangle? ")))
     rec(l, b)
 def parallelogram():
     from area import parallelogram as para
-    b = int(input("What is the length of the base? "))
-    h = int(input("What is the height of the shape? "))
+    b = int(input(_("What is the length of the base? ")))
+    h = int(input(_("What is the height of the shape? ")))
     para(b, h)
 def rhombus():
     from area import rhombus as rhombu
-    do = int(input("What is the length of the first diagonal? "))
-    ds = int(input("What is the length of the 2nd diagonal? "))
+    do = int(input(_("What is the length of the first diagonal? ")))
+    ds = int(input(_("What is the length of the 2nd diagonal? ")))
     rhombu(do, ds)
 def trapezium():
     from area import trapezium as trapezi
-    a = int(input("What is the length of the 1st set of parallel sides? "))
-    b = int(input("What is the length of the 2nd set of parallel sides? "))
-    h = int(input("What is the height of the trapezium? "))
+    a = int(input(_("What is the length of the 1st set of parallel sides? ")))
+    b = int(input(_("What is the length of the 2nd set of parallel sides? ")))
+    h = int(input(_("What is the height of the trapezium? ")))
     trapezi(a, b, h)
 def circle():
     from area import circle as circl
-    r = int(input("What is the radius of the circle? "))
+    r = int(input(_("What is the radius of the circle? ")))
     circl(r)
 def semicircle():
     from area import semicircle as semi
-    r = int(input("What is the radius of the semicircle? "))
+    r = int(input(_("What is the radius of the semicircle? ")))
     semi(r)
 def sector():
-    print("I can't figure out what `a' is for, it is cirsector() in area.py if anyone can help")
+    print(_("I can't figure out what `a' is for, it is cirsector() in area.py if anyone can help"))
     #r = int(input("What is the radius of the circular sector? "))
     #cirsector(r)
 def ring():
     from area import ring as myprecious
-    ro = int(input("What is the radius of the outer circle? "))
-    rs = int(input("What is the radius of the inner circle? "))
+    ro = int(input(_("What is the radius of the outer circle? ")))
+    rs = int(input(_("What is the radius of the inner circle? ")))
     myprecious(ro, rs)
 def ellipse():
     from area import ellipse as el
-    a = int(input("What is the length of the major axis? "))
-    b = int(input("What is the length of the minor axis? "))
+    a = int(input(_("What is the length of the major axis? ")))
+    b = int(input(_("What is the length of the minor axis? ")))
     el(a, b)
-print('''Options:
+print(_('''Options:
 1 - Equilateral triangle
 2 - Right angle triangle
 3 - Acute triangle
@@ -76,14 +76,14 @@ print('''Options:
 12 - Semicircle
 13 - Circular sector
 14 - Ring
-15 - Ellipse''')
+15 - Ellipse'''))
 while True:
     try:
-        choice = int(input("Please type one: "))
+        choice = int(input(_("Please type one: ")))
     except (ValueError, TypeError):
-        print("Please type an integer")
+        print(_("Please type an integer"))
     if choice == 7:
-        print("I was too lazy to change 7.")
+        print(_("I was too lazy to change 7."))
     elif choice == 1:
         equ_triangle()
         break
@@ -118,11 +118,11 @@ while True:
         semicircle()
         break
     elif choice == 13:
-        print("I cannot figure out what variable `a' is for, line 182 in `area.py' if anyone wants to help")
+        print(_("I cannot figure out what variable `a' is for, line 182 in `area.py' if anyone wants to help"))
         break
     elif choice == 14:
         ring() #my precious!
         break
     elif choice == 15:
         ellipse()
-        break   
+        break
