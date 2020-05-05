266f57380acebde67d8141f12df4befb8b139454
Thittussarom <ittussarom@TtRTech-PC>
2020-05-05 09:23:30 -0400

    fix

--- volInteractive.py
+++ volInteractive.py
@@ -1,60 +1,61 @@
 from volume import *
 def cuvol():
-    a = int(input("What length is the side of the cube? "))
+    a = int(input(_("What length is the side of the cube? ")))
     vol_cube(a)
 def cuboid():
-    b = int(input("What length is the breadth of the cuboid? "))
-    h = int(input("What length is the height of the triangle? "))
+    b = int(input(_("What length is the breadth of the cuboid? ")))
+    h = int(input(_("What length is the height of the cuboid? ")))
+    l = int(input(_("What length is the cuboid? ")))
     vol_cuboid(b=b, h=h, l=l)
 def cylindervol():
-    r = int(input("What is the radius of the cylinder? "))
-    h = int(input("what is the height of the cylinder? "))
+    r = int(input(_("What is the radius of the cylinder? ")))
+    h = int(input(_("what is the height of the cylinder? ")))
     vol_cylinder(r, h)
 def hollow_cylinder():
-    ro = int(input("What is the radius of the hollow space? "))
-    rs = int(input("what is the radius of the cylinder? "))
-    h = int(input("What is the height of the cylinder? "))
+    ro = int(input(_("What is the radius of the hollow space? ")))
+    rs = int(input(_("What is the radius of the cylinder? ")))
+    h = int(input(_("What is the height of the cylinder? ")))
     vol_hollow_cylinder(ro, rs, h)
 def cone():
-    r = int(input("What is the radius of the cone? "))
-    h = int(input("What is the height of the cone? "))
+    r = int(input(_("What is the radius of the cone? ")))
+    h = int(input(_("What is the height of the cone? ")))
     vol_cone(a)
 def sphere():
-    r = int(input("What is the radius of the rectangle? "))
+    r = int(input(_("What is the radius of the sphere? ")))
     vol_sphere(r)
 def hollow_sphere():
-    ro = int(input("What is the radius of the sphere? "))
-    rs = int(input("What is the radius of the hollow space? "))
+    ro = int(input(_("What is the radius of the sphere? ")))
+    rs = int(input(_("What is the radius of the hollow space? ")))
     vol_hollow_sphere(ro, rs)
 def triprism():
-    a = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the prism? "))
+    a = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the prism? ")))
     vol_tri_prism(a, h)
 def pentprism():
-    a = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the prism? "))
+    a = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the prism? ")))
     vol_penta_prism(a, h)
 def hexaprism():
-    r = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the prism? "))
+    r = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the prism? ")))
     vol_hexa_prism(r, h)
 def squiramid():
-    a = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the pyramid? "))
+    a = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the pyramid? ")))
     vol_sqr_pyramid(a, h)
 def triramid():
-    a = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the pyramid? "))
+    a = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the pyramid? ")))
     vol_tri_pyramid(a, h)
 def pentapyr():
-    ro = int(input("What is the length of the side of the base? "))
-    rs = int(input("What is the height of the pyramid? "))
+    ro = int(input(_("What is the length of the side of the base? ")))
+    rs = int(input(_("What is the height of the pyramid? ")))
     vol_penta_pyramid(ro, rs)
 def hexramid():
-    a = int(input("What is the length of the side of the base? "))
-    h = int(input("What is the height of the pyramid? "))
+    a = int(input(_("What is the length of the side of the base? ")))
+    h = int(input(_("What is the height of the pyramid? ")))
     vol_hexa_pyramid(a, h)
-print('''Options:
+print(_('''Options:
 1 - Cube
 2 - Cuboid
 3 - Cylinder
@@ -68,14 +69,14 @@ print('''Options:
 12 - Square-based pyramid
 13 - Triangular pyramid
 14 - Pentagon-based pyramid
-15 - Hexagon-based pyramid''')
+15 - Hexagon-based pyramid'''))
 while True:
     try:
-        choice = int(input("Please type one: "))
+        choice = int(input(_("Please type one: ")))
     except (ValueError, TypeError):
-        print("Please type an integer")
+        print(_("Please type an integer"))
     if choice == 7:
-        print("Sorry, that was not an option. >:)")
+        print(_("Sorry, that was not an option. >:)"))
     elif choice == 1:
         cuvol()
         break
