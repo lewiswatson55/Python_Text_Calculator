5a4d0d2a90948324784b4c83ed9ef79a6951e2ad
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-06 08:43:30 -0400

    comments, more log

--- palc.py
+++ palc.py
@@ -3,17 +3,17 @@
 # THANKS TO @ErdoganOnal for their comment on this SO question: https://stackoverflow.com/questions/61621821/any-secure-alternatives-for-this?noredirect=1#comment109002742_61621821
 # THANKS TO https://stackoverflow.com/questions/33594958/is-it-possible-to-align-a-print-statement-to-the-center-in-python
 # 
-import sys, os
-import os
+import sys, os, logging #sys so I can exit, os so I can do I can't remember, logging so I can log.
+logging.basicConfig(filename="palc.log", level=logging.DEBUG) #set up logging
 try: 
     import msvcrt 
     _IS_WINDOWS = True 
+    logger.info("Imported msvcrt")
 except ImportError: 
     import tty 
     import termios 
     _IS_WINDOWS = False 
-import logging #so I can log
-logging.basicConfig(filename="palc.log", level=logging.DEBUG) #set up logging
+    logging.info("Imported tty, termios")
 import gettext #to translate Palc
 language = input("English or Francais? (do not add accents to letters/ne pas ajouter les accents aux lettres): ")
 language = language.lower()
