c59c0e1123808b5c09a22ee9ebe940ef80d09519
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-06 13:53:41 -0400

    more stuff

--- fibonacci.py
+++ fibonacci.py
@@ -3,13 +3,19 @@ import time
 num0 = 0
 num1 = 1
 hi = 0
-while True:
-    num = num0 + num1
-    if hi == 0:
-        num0 = num
-        hi = 1
-    else:
-        num1 = num
-        hi = 0
-    print(num, end=", ", flush=True)
-    time.sleep(0.5)
+print("Press Control-C to stop.")
+try:
+    while True:
+        num = num0 + num1
+        if hi == 0:
+            num0 = num
+            hi = 1
+        else:
+            num1 = num
+            hi = 0
+        print(num, end=", ", flush=True)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Thanks for using Palc's FIBONACCI function!")
+except:
+    print("an error occured.")
