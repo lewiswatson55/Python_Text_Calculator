c0e57d28acba65cfe61a1fd26bf36a6de9d90b1a
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-09 19:28:50 -0400

    wip

---
+++ percent.py
@@ -0,0 +1,10 @@
+def percentage(percent, whole):
+    return (percent * whole) / 100.0
+def whatIsPercent():
+    origin = int(input("what is the ORIGINAL NUMBER? "))
+    percent = int(input("What is the PERCENTAGE? "))
+    percentage(percent, origin)
+def getPercentage(part, whole):
+    return 100 * float(part)/float(whole)
+def getPercentageRN():
+    origin = int(input(""))
