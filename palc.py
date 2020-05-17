ae1c5a9965c1013b375c22415e4fc1624180817d
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-17 12:03:00 -0400

    Added logging entry for 'Program stopped execution'.

--- palc.py
+++ palc.py
@@ -262,3 +262,5 @@ except:
     logging.info(("Printed ", width, "dashes"))
     cprint.fatal(_("An unknown error occured. Please file an Issue at github.com/thetechrobo/support."))
 #EOF
+finally:
+    logging.info("Program stopped execution.")
