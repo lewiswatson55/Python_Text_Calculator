acd28c38839a34955f7b8af80453b9e1267c796b
ittussaromYno3 <computer@Anfunfuns-MacBook-Air.local>
2020-06-06 17:33:25 -0400

    “fix”

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 import logging
 from cprint import cprint
 if __name__ == "__main__":
-    print(_("Please do not run any of these files directly. They don't do anything useful on their own."))
+    print("Please do not run any of these files directly. They don't do anything useful on their own.")
 def getNum(): #ask for two numbers and then return to function
     n1 = int(input(_("Please enter the first number: ")))
     n2 = int(input(_("Please enter the second number: ")))
